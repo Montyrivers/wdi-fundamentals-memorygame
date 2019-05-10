497e70956af4ac00c51280a0ccfedca2e96b73ee
ChrisM <chrismontalbo21@yahoo.com>
2019-05-10 18:24:05 -0400

    created game logic2

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,22 @@
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-console.log("User flipped " + cardOne + ' and ' + cardFour)
+// var cardOne = "Queen";
+// var cardTwo = "Queen";
+// var cardThree = "King";
+// var cardFour = "King";
+// console.log("User flipped " + cardOne + ' and ' + cardFour)
+
+const cards = ["Queen", "Queen", "King", "King"];
+const cardsInPlay = [];
+
+let cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+let cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+  if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a matching pair!"); 
+} else {
+	alert("Sorry, try again!");
+}
+}
